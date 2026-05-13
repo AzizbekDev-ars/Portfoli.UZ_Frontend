0c3f86aeaff9ebdff2182377668f9bc03df102d5
PocoC71 <pococ2228@gmail.com>
2026-05-13 14:38:24 +0500

    chore: setup API environment variables for Vercel deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000"; // Serveringiz portiga qarab o'zgartiring
+const API_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:4000"; // Serveringiz portiga qarab o'zgartiring
 
 const api = axios.create({
     baseURL: API_URL,
